d6db3093e4529eafa925d16b8c38c67c10f9f65c
greencomputinguae <greencomputinguae@gmail.com>
2018-10-19 11:34:37 +0200

    changing cal.py

--- calculate.py
+++ calculate.py
@@ -9,9 +9,9 @@ class calculation:
         return a-b
 
     def multiplication(a, b):
-        return a*b
+        return a**b
 
     def division(a, b):
         if b==0:
             raise ValueError("Cannot divid by 0 !")
-        return a/b
+        return a//b
